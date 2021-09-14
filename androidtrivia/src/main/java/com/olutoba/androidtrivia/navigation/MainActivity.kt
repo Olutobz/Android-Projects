3b104bfd3ee9dbddeb72d4a850bed01d5efe8183
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-09-14 18:13:57 +0100

    add fragments

--- androidtrivia/src/main/java/com/olutoba/androidtrivia/navigation/MainActivity.kt
+++ androidtrivia/src/main/java/com/olutoba/androidtrivia/navigation/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.olutoba.androidtrivia
+package com.olutoba.androidtrivia.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.olutoba.androidtrivia.R
+import com.olutoba.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
