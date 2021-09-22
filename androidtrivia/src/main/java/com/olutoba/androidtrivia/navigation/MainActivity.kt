049bfe9af8e9f5ad0523dd4a9900da38cd80e3b6
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-09-22 08:45:34 +0100

    use supportFragmentManager to get navController to setup actionBar with the  navigationUI

--- androidtrivia/src/main/java/com/olutoba/androidtrivia/navigation/MainActivity.kt
+++ androidtrivia/src/main/java/com/olutoba/androidtrivia/navigation/MainActivity.kt
@@ -3,8 +3,8 @@ package com.olutoba.androidtrivia.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.olutoba.androidtrivia.R
 import com.olutoba.androidtrivia.databinding.ActivityMainBinding
@@ -15,13 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val navController = this.findNavController(R.id.myNavHost)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
 
     override fun onNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHost)
+        val navController = this.findNavController(R.id.myNavHostFragment)
          return navController.navigateUp()
     }
 }
