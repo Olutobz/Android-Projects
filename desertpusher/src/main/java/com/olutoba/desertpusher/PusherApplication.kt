7e72391f06cffa9f7e2d4f5a28e6e2f460c431ff
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-10-15 09:20:16 +0100

    create custom application class

---
+++ desertpusher/src/main/java/com/olutoba/desertpusher/PusherApplication.kt
@@ -0,0 +1,12 @@
+package com.olutoba.desertpusher
+
+import android.app.Application
+import timber.log.Timber
+
+class PusherApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
