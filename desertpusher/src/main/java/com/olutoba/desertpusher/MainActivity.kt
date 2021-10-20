877ba16e22ac7e2e9f2cd081ba81b31842416150
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-10-20 17:23:23 +0100

    remove unused lifecycle callbacks

--- desertpusher/src/main/java/com/olutoba/desertpusher/MainActivity.kt
+++ desertpusher/src/main/java/com/olutoba/desertpusher/MainActivity.kt
@@ -79,36 +79,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     }
 
-    override fun onRestart() {
-        super.onRestart()
-        Timber.i("onRestart called")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Timber.i("onStart called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.i("onResume called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.i("onPause called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Timber.i("onDestroy called")
-    }
-
     /**
      * Updates the score when the dessert is clicked. Possibly shows a new dessert.
      */
