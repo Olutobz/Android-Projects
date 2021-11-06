342277c8c23263b46d1e2613735aac0162b8ce5d
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-06 15:38:36 +0100

    add sleeptracker, sleepquality class packages

---
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleepquality/SleepQualityFragment.kt
@@ -0,0 +1,40 @@
+package com.olutoba.sleepqualitytracker.sleepquality
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.olutoba.sleepqualitytracker.R
+import com.olutoba.sleepqualitytracker.databinding.FragmentSleepQualityBinding
+
+
+/**
+ * Fragment that displays a list of clickable icons,
+ * each representing a sleep quality rating.
+ * Once the user taps an icon, the quality is set in the current sleepNight
+ * and the database is updated.
+ */
+class SleepQualityFragment : Fragment() {
+
+    /**
+     * Called when the Fragment is ready to display content to the screen.
+     *
+     * This function uses DataBindingUtil to inflate R.layout.fragment_sleep_quality.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentSleepQualityBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_sleep_quality, container, false
+        )
+
+        val application = requireNotNull(this.activity).application
+
+        return binding.root
+    }
+}
---
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleepquality/SleepQualityViewModelFactory.kt
@@ -0,0 +1,1 @@
+package com.olutoba.sleepqualitytracker.sleepquality
