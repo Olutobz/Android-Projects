dca10710b4b991660b08b7f9bae0ebc166b992b2
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-12 19:12:33 +0100

    suppress the classTypeException warning in ScoreViewModelFactory

--- guesstheword/src/main/java/com/olutoba/guesstheword/viewmodel/ScoreViewModelFactory.kt
+++ guesstheword/src/main/java/com/olutoba/guesstheword/viewmodel/ScoreViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
             return ScoreViewModel(finalScore) as T
