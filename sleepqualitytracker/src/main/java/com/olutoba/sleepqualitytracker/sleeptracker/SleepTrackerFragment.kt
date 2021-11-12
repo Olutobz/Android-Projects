5b43243a52d66f647401e529efb7f9c0530d6e20
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-12 19:47:14 +0100

    create instance of the ViewModel and its ViewModelFactory

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleeptracker/SleepTrackerFragment.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleeptracker/SleepTrackerFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.olutoba.sleepqualitytracker.R
+import com.olutoba.sleepqualitytracker.database.SleepDatabase
 import com.olutoba.sleepqualitytracker.databinding.FragmentSleepTrackerBinding
 
 
@@ -25,6 +27,20 @@ class SleepTrackerFragment : Fragment() {
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sleep_tracker, container, false
         )
+
+        // Create an instance of the ViewModel Factory
+        val application = requireNotNull(this.activity).application
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        // Get a reference to the ViewModel associated with this fragment.
+        val sleepTrackerViewModel =
+            ViewModelProvider(this, viewModelFactory)[SleepTrackerViewModel::class.java]
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        // Specify the current activity as the lifecycle owner.
+        binding.lifecycleOwner = this.viewLifecycleOwner
 
         return binding.root
     }
