f3a1154f59397c87238b62e1b0574f3ad702ceb4
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-21 23:16:53 +0100

    add comments

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/database/SleepDatabaseDao.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/database/SleepDatabaseDao.kt
@@ -9,21 +9,36 @@ interface SleepDatabaseDao {
     @Insert
     fun insert(night: SleepNight)
 
+    /** When updating a row with a value already set in a colum, replaces
+     * the old value with the new one.
+     * @param night new value to write
+     * */
     @Update
     fun update(night: SleepNight)
 
+    /** Selects and returns the row that matches the supplied start time, which is our key
+     * @param key startTimeMilli to match
+     */
     @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
     fun get(key: Long): SleepNight?
 
     @Delete
     fun delete(night: SleepNight)
 
+    /** Deletes all values from the table.
+     * This does not delete the table, only its contents.
+     * */
     @Query("DELETE FROM daily_sleep_quality_table")
     fun clear()
 
+    /** Selects and returns all rows in the table,
+     * sorted by start time in descending order.
+     * */
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
     fun getAllNights(): LiveData<List<SleepNight>>
 
+    /** Selects and returns the latest night.
+     * */
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     fun getTonight(): SleepNight?
 
