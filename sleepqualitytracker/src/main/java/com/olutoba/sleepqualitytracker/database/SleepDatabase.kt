d2d9093d06bea8a45552ba73b61e0817714ca6a7
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-27 20:54:31 +0100

    add useful comments

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/database/SleepDatabase.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/database/SleepDatabase.kt
@@ -5,16 +5,45 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+/**
+ * A database that stores SleepNight information.
+ * And a global method to get access to the database.
+ */
 @Database(entities = [SleepNight::class], version = 1, exportSchema = false)
 abstract class SleepDatabase : RoomDatabase() {
 
+    /**
+     * Connects the database to the DAO.
+     */
     abstract val sleepDatabaseDao: SleepDatabaseDao
 
     companion object {
+        /**
+         * INSTANCE will keep a reference to any database returned via getInstance.
+         *
+         * This will help us avoid repeatedly initializing the database, which is expensive.
+         *
+         *  The value of a volatile variable will never be cached, and all writes and
+         *  reads will be done to and from the main memory. It means that changes made by one
+         *  thread to shared data are visible to other threads.
+         */
         @Volatile
         private var INSTANCE: SleepDatabase? = null
 
+        /**
+         * Helper function to get the database.
+         *
+         * If a database has already been retrieved, the previous database will be returned.
+         * Otherwise, create a new database.
+         *
+         * This function is threadsafe, and callers should cache the result for multiple database
+         * calls to avoid overhead.
+         * @param context The application context Singleton, used to get access to the filesystem.
+         */
         fun getInstance(context: Context): SleepDatabase {
+            // Multiple threads can ask for the database at the same time, ensure we only initialize
+            // it once by using synchronized. Only one thread may enter a synchronized block at a
+            // time.
             synchronized(this) {
                 var instance = INSTANCE
                 if (instance == null) {
@@ -23,6 +52,7 @@ abstract class SleepDatabase : RoomDatabase() {
                         SleepDatabase::class.java,
                         "sleep_history_database"
                     )
+                        // Wipes and rebuilds instead of migrating if no Migration object.
                         .fallbackToDestructiveMigration()
                         .build()
                     INSTANCE = instance
