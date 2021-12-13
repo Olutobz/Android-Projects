36d200ca0a8531eb22c1ab4135925f19668ba9ee
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-14 00:27:18 +0100

    implement the navigation from SleepDetailFragment back to the SleepTrackerFragment

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleepdetail/SleepDetailFragment.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleepdetail/SleepDetailFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.olutoba.sleepqualitytracker.R
+import com.olutoba.sleepqualitytracker.database.SleepDatabase
 import com.olutoba.sleepqualitytracker.databinding.FragmentSleepDetailBinding
 
 class SleepDetailFragment : Fragment() {
@@ -19,6 +22,29 @@ class SleepDetailFragment : Fragment() {
         val binding: FragmentSleepDetailBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sleep_detail, container, false
         )
+
+        val application = requireNotNull(this.activity).application
+        val arguments = SleepDetailFragmentArgs.fromBundle(requireArguments())
+
+        // Create an instance of the ViewModel Factory.
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepDetailViewModelFactory(arguments.sleepNightKey, dataSource)
+
+        val sleepDetailViewModel =
+            ViewModelProvider(this, viewModelFactory)[SleepDetailViewModel::class.java]
+
+        binding.sleepDetailViewModel = sleepDetailViewModel
+
+        // Specify the current activity as the lifecycle owner of the binding.
+        // This is necessary so that the binding can observe LiveData updates.
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        sleepDetailViewModel.navigateToSleepTracker.observe(viewLifecycleOwner, {
+            if (it == true) { // Observed state is true
+                findNavController().navigate(SleepDetailFragmentDirections.actionSleepDetailFragmentToSleepTrackerFragment())
+                sleepDetailViewModel.doneNavigating()
+            }
+        })
 
         return binding.root
     }
