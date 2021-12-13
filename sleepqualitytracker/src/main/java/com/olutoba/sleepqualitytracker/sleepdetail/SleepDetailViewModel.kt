3f2fd6a6610e752e2b3c1a911215ca68ca163d26
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-14 00:23:47 +0100

    Create the SleepDetailViewModel

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleepdetail/SleepDetailViewModel.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/sleepdetail/SleepDetailViewModel.kt
@@ -1,4 +1,46 @@
 package com.olutoba.sleepqualitytracker.sleepdetail
 
-class SleepDetailViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.olutoba.sleepqualitytracker.database.SleepDatabaseDao
+import com.olutoba.sleepqualitytracker.database.SleepNight
+import kotlinx.coroutines.Job
+
+
+class SleepDetailViewModel(
+    private val sleepNightKey: Long = 0L,
+    datasource: SleepDatabaseDao
+) : ViewModel() {
+
+    val database = datasource
+
+    private val viewModelJob = Job()
+
+    private val night = MediatorLiveData<SleepNight>()
+
+    fun getNight() = night
+
+    init {
+        night.addSource(database.getNightWithId(sleepNightKey), night::setValue)
+    }
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onClose() {
+        _navigateToSleepTracker.value = true
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
 }
