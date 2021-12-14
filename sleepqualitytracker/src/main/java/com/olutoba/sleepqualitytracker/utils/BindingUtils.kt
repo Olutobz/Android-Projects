30030c3685964686c36e6989a6f1a17c84d99f6c
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-14 23:28:19 +0100

    modified setSleepImage function implementation

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/utils/BindingUtils.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/utils/BindingUtils.kt
@@ -24,15 +24,17 @@ fun TextView.setSleepQualityString(item: SleepNight?) {
 
 @BindingAdapter("sleepImage")
 fun ImageView.setSleepImage(item: SleepNight?) {
-    setImageResource(
-        when (item?.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        }
-    )
+    item?.let {
+        setImageResource(
+            when (it.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+        )
+    }
 }
