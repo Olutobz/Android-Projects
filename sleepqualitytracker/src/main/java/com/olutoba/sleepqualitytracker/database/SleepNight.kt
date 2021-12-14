049ae58275d9b7b8715dbee36ae5df4e69cfa588
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-14 22:02:00 +0100

    modified SleepNight class

--- sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/database/SleepNight.kt
+++ sleepqualitytracker/src/main/java/com/olutoba/sleepqualitytracker/database/SleepNight.kt
@@ -6,7 +6,6 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight(
-
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "nightId")
     var id: Long = 0L,
@@ -15,7 +14,7 @@ data class SleepNight(
     var startTimeMilli: Long = System.currentTimeMillis(),
 
     @ColumnInfo(name = "end_time_milli")
-    var endTimeMilli: Long = System.currentTimeMillis(),
+    var endTimeMilli: Long = startTimeMilli,
 
     @ColumnInfo(name = "quality_rating")
     var sleepQuality: Int = -1
