8bfadec436fc131d7d710079b19f27280a857af3
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-17 05:03:37 +0100

    create viewModels

---
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/detail/DetailViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.olutoba.marsrealestate.detail
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.olutoba.marsrealestate.network.MarsProperty
+
+class DetailViewModelFactory(
+    private val marsProperty: MarsProperty,
+    private val application: Application
+) :
+    ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(marsProperty, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
