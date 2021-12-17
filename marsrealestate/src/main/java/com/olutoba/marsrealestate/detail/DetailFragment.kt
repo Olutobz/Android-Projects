a52ea864ee5e46801f71f21d45c062b000c25736
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-17 05:02:31 +0100

    modified both fragments

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/detail/DetailFragment.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/detail/DetailFragment.kt
@@ -1,20 +1,27 @@
-package com.olutoba.marsrealestate
+package com.olutoba.marsrealestate.detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.olutoba.marsrealestate.databinding.FragmentDetailBinding
 
 
+/**
+ * This [Fragment] will show the detailed information about a selected piece of Mars real estate.
+ */
 class DetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val application = requireNotNull(activity).application
+        val binding = FragmentDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
 }
