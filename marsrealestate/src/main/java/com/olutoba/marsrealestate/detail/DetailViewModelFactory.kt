3937b6f4d001e6e699c3827cd721be9a97877679
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-12-22 16:33:35 +0100

    modified details screen and viewmodelfactory

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/detail/DetailViewModelFactory.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/detail/DetailViewModelFactory.kt
@@ -8,8 +8,8 @@ import com.olutoba.marsrealestate.network.MarsProperty
 class DetailViewModelFactory(
     private val marsProperty: MarsProperty,
     private val application: Application
-) :
-    ViewModelProvider.Factory {
+) : ViewModelProvider.Factory {
+
     @Suppress("unchecked_cast")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
