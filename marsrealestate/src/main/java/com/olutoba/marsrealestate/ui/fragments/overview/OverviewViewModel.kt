5d6426b99bb308def504ed3c644e85333114c9fe
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-01-13 18:35:05 +0100

    fetch mars api service response

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewViewModel.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewViewModel.kt
@@ -3,11 +3,15 @@ package com.olutoba.marsrealestate.ui.fragments.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.olutoba.marsrealestate.network.MarsApi
+import retrofit2.Call
+import retrofit2.Response
 
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
     private val _response = MutableLiveData<String>()
+
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
         get() = _response
@@ -23,7 +27,15 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        MarsApi.retrofitApiService.getProperties().enqueue(object : retrofit2.Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+        })
     }
 
 }
