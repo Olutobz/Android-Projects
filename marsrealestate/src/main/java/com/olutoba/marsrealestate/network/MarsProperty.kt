6412fd8b6ee4296a3910d3ca63657c1aa5073b0c
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-01-14 18:40:42 +0100

    add kotlin data class with properties that matches the JSON response fields

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsProperty.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsProperty.kt
@@ -1,4 +1,10 @@
 package com.olutoba.marsrealestate.network
 
-class MarsProperty {
-}
+import com.squareup.moshi.Json
+
+data class MarsProperty(
+    val id: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    val price: Double
+)
