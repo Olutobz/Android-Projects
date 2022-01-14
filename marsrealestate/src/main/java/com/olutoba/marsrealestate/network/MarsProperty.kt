afa5a24ede7bcade5334cd18e28ebd0acbd7d343
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-01-14 19:02:31 +0100

    update MarsProperty data class

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsProperty.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsProperty.kt
@@ -4,7 +4,8 @@ import com.squareup.moshi.Json
 
 data class MarsProperty(
     val id: String,
-    @Json(name = "img_src") val imgSrcUrl: String,
+    @Json(name = "img_src")
+    val imgSrcUrl: String,
     val type: String,
     val price: Double
 )
