46dce67588e505d18cd1a7d938bacfb961a31812
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-01-21 18:54:07 +0100

    add bindImage function

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/PhotoGridAdapter.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/PhotoGridAdapter.kt
@@ -1,4 +1,25 @@
 package com.olutoba.marsrealestate.ui.fragments.overview
 
-class PhotoGridAdapter {
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.olutoba.marsrealestate.R
+
+
+/** Use the Glide library to load an image by URL into an ImageView */
+@BindingAdapter("imageUrl")
+fun bindImage(imageView: ImageView, imageUrl: String?) {
+    imageUrl?.let {
+        val imageUri = it.toUri().buildUpon().scheme("https").build()
+        Glide.with(imageView.context)
+            .load(imageUri)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(imageView)
+    }
 }
