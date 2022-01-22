845f18a3e1c3be403ff3f612203797d1fd64a0d0
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-01-22 22:56:42 +0100

    modified detail viewmodel

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/detail/DetailViewModel.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/detail/DetailViewModel.kt
@@ -2,7 +2,7 @@ package com.olutoba.marsrealestate.ui.fragments.detail
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.olutoba.marsrealestate.network.MarsProperty
+import com.olutoba.marsrealestate.networking.MarsProperty
 
 class DetailViewModel(marsProperty: MarsProperty, application: Application) :
     AndroidViewModel(application) {
