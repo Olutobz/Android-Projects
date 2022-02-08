bc372409b1da614fbf83ee9ca40c3fbd6510585e
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-08 22:35:51 +0100

    update ui

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewViewModel.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewViewModel.kt
@@ -15,9 +15,9 @@ class OverviewViewModel : ViewModel() {
     val status: LiveData<String>
         get() = _status
 
-    private val _property = MutableLiveData<MarsProperty>()
-    val property: LiveData<MarsProperty>
-        get() = _property
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+    val property: LiveData<List<MarsProperty>>
+        get() = _properties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -37,7 +37,7 @@ class OverviewViewModel : ViewModel() {
                 val listResult = MarsApi.retrofitApiService.getProperties()
                 _status.value = "Success: ${listResult.size} Mars properties retrieved"
                 if (listResult.isNotEmpty()) {
-                    _property.value = listResult[0]
+                    _properties.value = listResult
                 }
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}}"
