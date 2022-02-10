8f12c714f0abe23a6b316f78c4600640cd8cb8f6
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-10 16:51:16 +0100

    implemented PhotoGridAdapter for recyclerview

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/PhotoGridAdapter.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/PhotoGridAdapter.kt
@@ -1,1 +1,44 @@
 package com.olutoba.marsrealestate.ui.fragments.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.olutoba.marsrealestate.databinding.GridViewItemBinding
+import com.olutoba.marsrealestate.network.MarsProperty
+
+class PhotoGridAdapter :
+    ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsProperty>() {
+        override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+
+    class MarsPropertyViewHolder(private var binding: GridViewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(marsProperty: MarsProperty) {
+            binding.property = marsProperty
+            binding.executePendingBindings()
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder {
+        return MarsPropertyViewHolder(GridViewItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
+        val marsProperty = getItem(position)
+        holder.bind(marsProperty)
+    }
+
+}
