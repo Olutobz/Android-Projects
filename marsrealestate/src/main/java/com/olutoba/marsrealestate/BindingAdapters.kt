9cf2f3ea0640f780ea695778891eb15c3343f1a6
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-10 20:06:53 +0100

    add marsApiStatus to bindStatus

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/BindingAdapters.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package com.olutoba.marsrealestate
 
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.olutoba.marsrealestate.network.MarsProperty
+import com.olutoba.marsrealestate.ui.fragments.overview.MarsApiStatus
 import com.olutoba.marsrealestate.ui.fragments.overview.PhotoGridAdapter
 
 
@@ -31,4 +33,21 @@ fun bindImage(imageView: ImageView, imageUrl: String?) {
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, apiStatus: MarsApiStatus?) {
+    when (apiStatus) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        else -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
 }
