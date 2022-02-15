76489b19a3904820f583331cd4bf7fb7f1da125d
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-15 21:52:09 +0100

    navigate to selected mars property

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewFragment.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.olutoba.marsrealestate.R
 import com.olutoba.marsrealestate.databinding.FragmentOverviewBinding
 
@@ -31,7 +32,18 @@ class OverviewFragment : Fragment() {
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
-        binding.photosGrid.adapter = PhotoGridAdapter()
+        binding.photosGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
+            viewModel.displayPropertyDetails(it)
+        })
+
+        viewModel.navigateToSelectedProperty.observe(viewLifecycleOwner) {
+            if (it != null) {
+                this.findNavController()
+                    .navigate(OverviewFragmentDirections.actionOverviewFragmentToDetailFragment(it))
+                viewModel.displayPropertyDetailsComplete()
+            }
+        }
+
         setHasOptionsMenu(true)
         return binding.root
     }
--- marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewViewModel.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/ui/fragments/overview/OverviewViewModel.kt
@@ -25,6 +25,10 @@ class OverviewViewModel : ViewModel() {
     val properties: LiveData<List<MarsProperty>>
         get() = _properties
 
+    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty?>()
+    val navigateToSelectedProperty: LiveData<MarsProperty?>
+        get() = _navigateToSelectedProperty
+
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -53,4 +57,14 @@ class OverviewViewModel : ViewModel() {
             }
         }
     }
+
+    fun displayPropertyDetails(marsProperty: MarsProperty) {
+        _navigateToSelectedProperty.value = marsProperty
+    }
+
+
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
+    }
+
 }
