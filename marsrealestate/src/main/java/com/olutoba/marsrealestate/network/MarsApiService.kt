6138b4b0b6727af57a28a1496d9c6dccea24fe6e
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-15 23:27:19 +0100

    add MarsApiFilter

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsApiService.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsApiService.kt
@@ -5,8 +5,15 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://mars.udacity.com/"
+
+enum class MarsApiFilter(val value: String) {
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SH0W_ALL("all")
+}
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -21,7 +28,7 @@ private val retrofit = Retrofit.Builder()
 requested with the GET HTTP method */
 interface MarsApiService {
     @GET("realestate")
-    suspend fun getProperties(): List<MarsProperty>
+    suspend fun getProperties(@Query("filter") type: String): List<MarsProperty>
 }
 
 object MarsApi {
