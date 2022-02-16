fd4113f3a76980732f106ed3d268f38cef796c5a
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-02-16 08:26:38 +0100

    removed deprecated import class

--- marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsProperty.kt
+++ marsrealestate/src/main/java/com/olutoba/marsrealestate/network/MarsProperty.kt
@@ -2,7 +2,8 @@ package com.olutoba.marsrealestate.network
 
 import android.os.Parcelable
 import com.squareup.moshi.Json
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
+
 
 @Parcelize
 data class MarsProperty(
