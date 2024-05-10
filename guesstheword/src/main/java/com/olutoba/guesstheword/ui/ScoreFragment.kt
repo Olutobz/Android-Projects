f7709157d41d4c78fe07994212e741ec8ea0bf9e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-05-10 01:07:15 +0100

    modify ScoreFragment

--- guesstheword/src/main/java/com/olutoba/guesstheword/ui/ScoreFragment.kt
+++ guesstheword/src/main/java/com/olutoba/guesstheword/ui/ScoreFragment.kt
@@ -18,28 +18,24 @@ import com.olutoba.guesstheword.viewmodel.ScoreViewModelFactory
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
-
     private lateinit var viewModelFactory: ScoreViewModelFactory
     private lateinit var viewModel: ScoreViewModel
-
+    private val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+    private lateinit var binding: ScoreFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        // Inflate view and obtain an instance of the binding class.
-        val binding: ScoreFragmentBinding =
+        binding =
             DataBindingUtil.inflate(inflater, R.layout.score_fragment, container, false)
 
-        // Get args using by navArgs property delegate
-        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
         viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
         viewModel = ViewModelProvider(this, viewModelFactory)[ScoreViewModel::class.java]
         binding.scoreViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        viewModel.eventPlayAgain.observe(viewLifecycleOwner) { playAgain ->
-            if (playAgain) {
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner) {
+            if (it) {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
                 viewModel.onPlayAgainComplete()
             }
