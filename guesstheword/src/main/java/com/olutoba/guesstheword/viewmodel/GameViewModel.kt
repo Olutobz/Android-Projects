0410f48c8aaab95b434eb435346c3d9381132b8b
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-05-17 04:26:53 +0100

    modify GameViewModel

--- guesstheword/src/main/java/com/olutoba/guesstheword/viewmodel/GameViewModel.kt
+++ guesstheword/src/main/java/com/olutoba/guesstheword/viewmodel/GameViewModel.kt
@@ -104,13 +104,13 @@ class GameViewModel : ViewModel() {
 }
 
 enum class BuzzType(val pattern: LongArray) {
-    CORRECT(CORRECT_BUZZ_PATTERN),
-    GAME_OVER(GAME_OVER_BUZZ_PATTERN),
-    COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
-    NO_BUZZ(NO_BUZZ_PATTERN)
+    CORRECT(correctBuzzPattern),
+    GAME_OVER(gameOverBuzzPattern),
+    COUNTDOWN_PANIC(panicBuzzPattern),
+    NO_BUZZ(noBuzzPattern)
 }
 
-private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
-private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
-private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
-private val NO_BUZZ_PATTERN = longArrayOf(0)
+private val correctBuzzPattern = longArrayOf(100, 100, 100, 100, 100, 100)
+private val panicBuzzPattern = longArrayOf(0, 200)
+private val gameOverBuzzPattern = longArrayOf(0, 2000)
+private val noBuzzPattern = longArrayOf(0)
